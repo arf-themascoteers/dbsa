bc0f9ad0c7eb3132933b06fb73de105d412803a1
Arif <arif@themascoteers.com>
2023-11-16 23:53:57 +1100

    ignored

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                 for random_initialize in [True, False]:
                     for uniform_lr in [True, False]:
                         for skip in [True, False]:
-                            for lr in [0.0001, 0.001, 0.01, 0.1]:
+                            for lr in [0.0001]:#, 0.001, 0.01, 0.1]:
                                 ds = DSManager(dwt)
                                 train_x, train_y, test_x, test_y, validation_x, validation_y = ds.get_datasets()
                                 print(f"Train: {len(train_y)}, Test: {len(test_y)}, Validation: {len(validation_y)}")
