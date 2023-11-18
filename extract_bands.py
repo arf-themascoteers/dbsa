4c873eea1582f2bc2d0aa13bc933e4c3a77017c3
Arif <arif@themascoteers.com>
2023-11-18 21:01:42 +1100

    ignored

--- extract_bands.py
+++ extract_bands.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("archive/True_sigmoid_False_False_False_0.0001_False.csv")
+df = pd.read_csv("results/True_sigmoid_False_True_True_0.001_False.csv")
 first_row_band_columns = df.loc[0, df.columns[df.columns.str.startswith('band')]]
 ar = [f"{a}" for a in first_row_band_columns]
 print(ar)
