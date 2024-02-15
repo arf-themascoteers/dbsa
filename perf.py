778792464cab309f6a290caaefda616c4f445d2b
Arif <arif@themascoteers.com>
2024-02-16 00:13:44 +1100

    ignored

--- perf.py
+++ perf.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("results/results.csv")
+#df = pd.read_csv("archive/4/results.csv")
+df = pd.read_csv("archive/3/results.csv")
 result = df.groupby('sis')['r2_test'].mean()
 print(result)
 
--- test_single.py
+++ test_single.py
@@ -9,44 +9,27 @@ from gndi2 import GNDI2
 from gndi3 import GNDI3
 from gndi4 import GNDI4
 from gndi2alpha import GNDI2Alpha
+from gndi3alpha import GNDI3Alpha
+from gndi4alpha import GNDI4Alpha
 import torch
 
 if __name__ == '__main__':
     features = [4200]
     samples = [21782]
-    siss = [
+    siss = [ ]
+    for i in range(10):
+        siss.append([{"si": GNDI2, "count": 1}])
+    for i in range(10):
+        siss.append([{"si": GNDI3, "count": 1}])
+    for i in range(10):
+        siss.append([{"si": GNDI4, "count": 1}])
+    for i in range(10):
+        siss.append([{"si": GNDI2Alpha, "count": 1}])
+    for i in range(10):
+        siss.append([{"si": GNDI3Alpha, "count": 1}])
+    for i in range(10):
+        siss.append([{"si": GNDI4Alpha, "count": 1}])
 
-        [
-            {"si": GNDI3, "count": 1}
-        ],
-        [
-            {"si": GNDI3, "count": 1}
-        ],
-        [
-            {"si": GNDI3, "count": 1}
-        ],
-        [
-            {"si": GNDI3, "count": 1}
-        ],
-        [
-            {"si": GNDI3, "count": 1}
-        ],
-        [
-            {"si": GNDI3, "count": 1}
-        ],
-        [
-            {"si": GNDI3, "count": 1}
-        ],
-        [
-            {"si": GNDI3, "count": 1}
-        ],
-        [
-            {"si": GNDI3, "count": 1}
-        ],
-        [
-            {"si": GNDI3, "count": 1}
-        ]
-    ]
 
     tasks = []
     for feature in features:
