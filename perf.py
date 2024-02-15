13aca548c87a977f517cdb01da7a69663d4e0ccd
Arif <arif@themascoteers.com>
2024-02-15 21:45:40 +1100

    ignored

--- perf.py
+++ perf.py
@@ -2,5 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("results/results.csv")
 result = df.groupby('sis')['r2_test'].mean()
+print(result)
 
+df = df[df["r2_test"] > 0.3]
+result = df.groupby('sis')['r2_test'].mean()
 print(result)
